604df276e8c50e799e6dadb7c0011b0a9b1cb206
Сергей Крюков <krus-83@mail.ru>
2021-04-13 16:32:22 +0700

    store selector isSubmittingSelector for Auth module

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Store} from '@ngrx/store';
+import {select, Store} from '@ngrx/store';
 import {registerAction} from 'src/app/auth/store/actions/register.action';
+import {Observable} from 'rxjs';
+import {isSubmittingSelector} from 'src/app/auth/store/selectors';
 
 @Component({
   selector: 'mc-register',
@@ -10,6 +12,7 @@ import {registerAction} from 'src/app/auth/store/actions/register.action';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
@@ -19,6 +22,12 @@ export class RegisterComponent implements OnInit {
      fetch data, initialize listeners, initialize variables, initialize form
      */
     this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    console.log(this.isSubmitting$);
   }
 
   initializeForm(): void {
