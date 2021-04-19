c11b134ad3dfe21e5088c316ff6307d1f2132793
Sergey Kryukov <krus-83@mail.ru>
2021-04-19 21:51:23 +0700

    store selectors for feed

--- src/app/shared/types/appState.interface.ts
+++ src/app/shared/types/appState.interface.ts
@@ -1,6 +1,8 @@
 import {AuthStateInterface} from 'src/app/shared/types/authState.interface';
+import {FeedStateInterface} from '../modules/feed/types/feedState.interface';
 
 // Данный интерфейс будет нам показывать все состояние приложения
 export interface AppStateInterface {
   auth: AuthStateInterface;
+  feed: FeedStateInterface;
 }
